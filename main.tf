f7443ed9f43623849c0be2ccd0444568b8313b6e
pranjal124 <84303554+pranjal124@users.noreply.github.com>
2024-06-27 10:03:52 +0530

    Update userdata main.tf

--- main.tf
+++ main.tf
@@ -8,9 +8,20 @@ terraform{
 provider "aws"{
  access_key = "AKIA6DGDC6XBIVIMGWP3"
  secret_key = "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"
- region = "us-west-1"
+ region = "us-west-2"
 }
 resource "aws_instance" "web" {
-  ami = "ami-08012c0a9ee8e21c4"
+  ami = "ami-0cf2b4e024cdb6960"
   instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y httpd
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+              sudo echo "DevOps Aviva Assignment" > /var/www/html/index.html
+              EOF
+}
+output "public_ip" {
+   value = aws_instance.web.public_ip 
 }
