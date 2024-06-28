11150241a469cbb99005dd8d34cf3d8cb14c4879
pranjal124 <84303554+pranjal124@users.noreply.github.com>
2024-06-28 10:19:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,17 +10,18 @@ provider "aws"{
  secret_key = "y1SzVnKJ8lb7rEchbWnrJ2OdIOKuxSMVSY5D4sVE"
  region = "us-west-1"
 }
+resource "aws_key_pair" "tf_key" {
+  key_name   = "tf-key"
+  public_key = file("${path.module}/rsa.pub")
+}
 resource "aws_instance" "web" {
   ami = "ami-08012c0a9ee8e21c4"
   instance_type = "t2.micro"
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y httpd
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              sudo echo "DevOps Aviva Assignment" > /var/www/html/index.html
-              EOF
+  key_name = "${aws_key_pair.tf_key.key_name}"
+  user_data = file("${path.module}/script.sh")
+}
+output "public_ip" {
+   value = aws_instance.web.public_ip 
 }
 output "public_ip" {
    value = aws_instance.web.public_ip 
